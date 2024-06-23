f87073f86d7dbfdbcadc672c5847af7063545b15
Burhan <burhan.ahmed60090@gmail.com>
2024-06-24 00:28:51 +0500

    Merge  changes

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ This is a Internship task done by me at Bytewise.
 - Navlink and Link also work the same but Navlink provides with an additional attribute. (ActiveClass)
 - When you use flex then you should use space-x-5 or space-y-5 and when you use grid then you should use gap-5
 - Basically, this is because Tailwind CSS provides specific utility classes designed for different layout systems.
+- This Project also involves the Creation of custom API using `json-server`.
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
                 <ul className=' flex text-2xl justify-around py-8 border border-black'>
                     <NavLink to="/">
                         <div className='flex'>
-                            <img src="/src/assets/react.png" alt="React Logo" width={60} />
+                            <img src="react.png" alt="React Logo" width={60} />
                             <div className='pt-3.5 ps-1 text-3xl'>
                                 React Jobs
                             </div>
