d160cdcd7f86ca152e5b0a7c6109407c631b3b80
Burhan <burhan.ahmed60090@gmail.com>
2024-06-26 13:26:28 +0500

    Merge  changes

---
+++ README.md
@@ -0,0 +1,3 @@
+# About Context APIs
+
+- This task help me to dive deep into the world of APIs
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Cart from './components/cart'
+import Items from './components/Item'
+
+export default function App() {
+
+
+  return (
+    <div >
+      <Items Product='Macbook Pro' Price='1000' />
+      <Items Product='OnePlus 9 Pro' Price='200' />
+      <Items Product='Samsung S21 Ultra' Price='800' />
+      <Items Product='Iphone 13' Price='450' />
+      <Cart />
+    </div>
+  )
+}
