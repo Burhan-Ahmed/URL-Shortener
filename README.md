cc186a278640c77f916ea8c863740e7fca94ff7c
Burhan <burhan.ahmed60090@gmail.com>
2024-07-17 16:18:02 +0500

    Initial commit

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ A task for the leaning of ExpressJs during my internship
 ## Point to Ponder
 - Since I am working on backend and do not have a UI. Thus using ejs for Server-Side Rendering.
 - Sync(Blocking "writeFileSync") mean that everything is done step by step Async(Non-Blocking "writeFile") mean that everything is done without stoping the execution even if a function take more time to excute.
+- Use of callback function is nesscary while working with .writeFile()
 - The server indeed doesn't "request" in the traditional sense. Instead, it listens for incoming requests from clients.
 - .get mean to get data from some resource and .post mean to send data to some resource.
 - Use ejs to get a ready to go UI for your Backend Envirnment.
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const fs = require('fs')
 const data = require('./Data/MOCK_DATA.json');
 
 app.use(express.json());
@@ -23,7 +24,39 @@ app.post("/", (req, res) => {
 })
 
 app.get('/', (req, res) => {
-    return res.render('inputinfo');
+    return res.render('login');
+});
+
+app.post("/signup.ejs", (req, res) => {
+
+    const postData1 = req.body.fnam;
+    const pass1 = req.body.pass1;
+    const pass2 = req.body.pass2;
+
+    if (pass1 != pass2) {
+        res.send("Password does not Match")
+        form.reset();
+    }
+    else {
+        const newuser = {
+            id: data.length + 1,
+            Name: postData1,
+            password: pass1
+        }
+        data.push(newuser)
+
+        fs.writeFile('./Data/MOCK_DATA.json', JSON.stringify(data), (err) => {
+            if (err) {
+                return res.status(500).send("Error saving data");
+            } else {
+                res.send("Account Created")
+            }
+        })
+    }
+})
+
+app.get('/signup.ejs', (req, res) => {
+    return res.render('signup');
 });
 
 app.listen(port, () => {
