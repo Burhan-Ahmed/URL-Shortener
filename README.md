58f1c42c8141480f5519b4061ac95e33ecc6bb22
Burhan <burhan.ahmed60090@gmail.com>
2024-07-24 03:34:44 +0500

    Initial commit

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
-# User-Auth-App
-A task for the leaning of ExpressJs during my internship
+# URL-Shortener
+Done this task to learn about Mongo and Express during my Internship at Bytewise
+![Alt text](public/bg.png)
 
 ## Tools and Language Used
 - MongoDB
 - Express
-- Postman
+- EJS (Embedded JavaScript templates)
+
+## Working Steps
+1. Enter your URL
+2. Click the Generate Button to Create Shorten URL
+3. use the Generated URL like http://localhost:4000/(Shorten URL)
 
 ## Point to Ponder
 - MongoDB: NoSQL database storing data in JSON-like documents.
 - MongoDB Compass: GUI for exploring and managing MongoDB data.
 - MongoDB Atlas: Cloud-based, fully managed MongoDB service.
 - Mongoose: ODM library for MongoDB in Node.js.
-- MongoDB Shell (mongosh): Command-line interface for MongoDB.
+- MongoDB Shell (mongosh): Command-line interface for MongoDB.3
+- Make sure to use Middlewares for proper connections while working on server
+- <%= text %>: Outputs the value of the expression. Used to render data from your server.
+- <% text %>: Executes JavaScript code. It is used for as loops and conditionals.
--- app.js
+++ app.js
@@ -1,19 +1,41 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
-const port = 4000
+const port = 4000;
+const path = require('path');
+const Url = require('./models/urlData');
+
 app.set('view engine', 'ejs');
+app.use('/', express.static(path.join(__dirname, '/')));
+app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect('mongodb://localhost/urlData')
-
-app.post('/', (req, res) => {
-
+mongoose.connect('mongodb://localhost/urlData', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 20000
+}).then(() => {
+    console.log('MongoDB Connected');
 })
 
-app.get('/', (req, res) => {
-    return res.render('home')
-})
+app.post('/', async (req, res) => {
+    const Urldata = await Url.create({ full: req.body.urlLink });
+    // console.log(urldata)
+    res.redirect('/');
+});
+
+app.get('/', async (req, res) => {
+    const data = await Url.find().exec();
+    console.log(data)
+    res.render('home', { data });
+});
+
+app.get('/:short', async (req, res) => {
+    const small = await Url.findOne({ short: req.params.short })
+    small.clicks++;
+    small.save();
+    res.redirect(small.full);
+});
 
 app.listen(port, () => {
-    console.log('Server is live at ', port)
-})
+    console.log('Server is live at', port);
+});
